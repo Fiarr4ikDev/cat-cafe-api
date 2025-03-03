f04ad967f68eba9b3951feaf41c8086c9ed4a544
fiarr4ik <fiarr4ik@gmail.com>
2025-03-03 13:52:41 +0700

    init commit

---
+++ src/main/java/ru/fiarr4ikdev/catcafeapi/CatCafeApiApplication.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ikdev.catcafeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatCafeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatCafeApiApplication.class, args);
+	}
+
+}
