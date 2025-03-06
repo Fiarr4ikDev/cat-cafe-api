abfb10a262e17867f7ac65d61516e7b25fc61971
fiarr4ik <fiarr4ik@gmail.com>
2025-03-06 17:10:55 +0700

    commit 06.03.2025 17:10 fix: поменял названия url для большей ясности и возможности расширения feat: пометил контроллер CoffeeController аннотациями для документации через swagger-ui: Operation - для пояснения того, что делает данный http метод Tag - для того чтобы задать имя контроллеру

--- src/main/java/ru/fiarr4ikdev/catcafeapi/controller/CoffeeController.java
+++ src/main/java/ru/fiarr4ikdev/catcafeapi/controller/CoffeeController.java
@@ -1,5 +1,7 @@
 package ru.fiarr4ikdev.catcafeapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +18,13 @@ import ru.fiarr4ikdev.catcafeapi.service.impl.CoffeeService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cat-cafe/api")
+@RequestMapping(CoffeeController.BASE_URL)
+@Tag(
+    name = "Контроллер для управления сущностью Coffee"
+)
 public class CoffeeController {
+
+    protected static final String BASE_URL = "/api/coffee";
 
     private final CoffeeService coffeeService;
 
@@ -26,22 +33,26 @@ public class CoffeeController {
         this.coffeeService = coffeeService;
     }
 
+    @Operation(summary = "Добавить запись Coffee в базу данных")
     @PostMapping("/add")
     public ResponseEntity<CoffeeDTO> saveCoffee(@RequestBody CoffeeDTO coffee) {
         return new ResponseEntity<>(coffeeService.save(coffee), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Показать все записи Coffee в виде листа содержащего объекты CoffeeDTO")
     @GetMapping("/all")
     public ResponseEntity<List<CoffeeDTO>> getAllCoffees() {
         return new ResponseEntity<>(coffeeService.findAll(), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
+    @Operation(summary = "Получить запись Coffee по его id")
+    @GetMapping("get/{id}")
     public ResponseEntity<CoffeeDTO> getCoffee(@PathVariable long id) {
         return new ResponseEntity<>(coffeeService.findOne(id), HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}")
+    @Operation(summary = "Удалить запись Coffee по его id")
+    @DeleteMapping("delete/{id}")
     public ResponseEntity<Void> deleteCoffee(@PathVariable long id) {
         coffeeService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
